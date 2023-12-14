36748bd606db913fadd383838c287703b5d396eb
Tobias Wupperfeld <66420306+tobiaswu@users.noreply.github.com>
2023-12-14 17:38:48 -0300

    project setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  daisyui: {
+    themes: [
+      {
+        darkTheme: {
+          primary: '#55d44c',
+          secondary: '#4059ad',
+          accent: '#cc3f0c',
+          neutral: '#252d2a',
+          'base-100': '#121612',
+          info: '#0089ff',
+          success: '#1dffba',
+          warning: '#f7c400',
+          error: '#ce2854',
+        },
       },
-    },
+    ],
   },
-  plugins: [],
-}
-export default config
+  plugins: [require('daisyui')],
+};
+export default config;
