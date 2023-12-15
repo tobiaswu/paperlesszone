22ea6861fcdc50d9e515c9adc033db03588b9e76
Tobias Wupperfeld <66420306+tobiaswu@users.noreply.github.com>
2023-12-15 12:11:21 -0300

    feat: footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import { PiDotOutlineLight, PiTwitterLogoLight } from 'react-icons/pi';
+
+export const Footer = () => {
+  return (
+    <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+      <aside className="items-center grid-flow-col">
+        <p>© 2023 TW Software Solutions LLC</p>
+        <PiDotOutlineLight />
+        <Link className="hover:text-primary" href="/">
+          <p>Privacy</p>
+        </Link>
+        <PiDotOutlineLight />
+        <Link className="hover:text-primary" href="/">
+          <p>Terms</p>
+        </Link>
+      </aside>
+      <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+        <Link href="/">
+          <PiTwitterLogoLight className="hover:text-primary text-3xl" />
+        </Link>
+      </nav>
+    </footer>
+  );
+};
