3374192c632879e64e5f3621615f748790d447db
Tobias Wupperfeld <66420306+tobiaswu@users.noreply.github.com>
2023-12-16 14:43:41 -0300

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
