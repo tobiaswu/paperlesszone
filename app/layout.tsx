b8dbe9a6d5d1f65a0b5615c19483505db8f3d426
Tobias Wupperfeld <66420306+tobiaswu@users.noreply.github.com>
2023-12-16 14:28:50 -0300

    feat: add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <SpeedInsights />
       <body className={inter.className}>
         <Header />
         {children}
