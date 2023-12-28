4a7c0c9ca05180a863f91425b71cf2eb5e48b00a
Tobias Wupperfeld <66420306+tobiaswu@users.noreply.github.com>
2023-12-28 08:48:16 -0300

    feat(contact): add animations

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,29 +1,48 @@
+'use client';
+
 import { ContactForm } from '@/components/ContactForm';
 import { ContactOption } from '@/components/ContactOption';
+import { motion } from 'framer-motion';
 import {
   PiChatDotsLight,
   PiMapPinLineLight,
   PiPhoneCallLight,
 } from 'react-icons/pi';
+import transitions from '@/public/transitions.json';
 
 export default function Contact() {
   return (
     <div className="container mx-auto bg-base-100">
       <div className="grid grid-cols-2 gap-12 px-4 py-16 min-h-screen place-items-center">
         <div className="col-span-2 lg:col-span-1">
-          <h1 className="text-4xl sm:text-6xl font-bold mb-12 sm:leading-relaxed">
+          <motion.h1
+            variants={transitions.item}
+            initial="hidden"
+            whileInView="show"
+            className="text-4xl sm:text-6xl font-bold mb-12 sm:leading-relaxed"
+          >
             Get in touch
-          </h1>
-          <p className="leading-relaxed mb-8">
+          </motion.h1>
+          <motion.p
+            variants={transitions.item}
+            initial="hidden"
+            whileInView="show"
+            className="leading-relaxed mb-8"
+          >
             Got a project in mind? Let&apos;s talk, we&apos;d love to hear from
             you. Choose a contact option below or simply fill in the form and
             one of the team will be in touch!
-          </p>
-          <div className="flex flex-col gap-4">
+          </motion.p>
+          <motion.div
+            variants={transitions.container}
+            initial="hidden"
+            whileInView="show"
+            className="flex flex-col gap-4"
+          >
             <ContactOption
               icon={<PiChatDotsLight />}
               optionLabel="Email"
-              optionText="hello@aiaa.com"
+              optionText="info@digitizerspace.com"
               url="mailto:"
             />
             <ContactOption
@@ -37,19 +56,24 @@ export default function Contact() {
               optionLabel="Office"
               optionText="USA, Florida (global clients)"
             />
-          </div>
+          </motion.div>
         </div>
 
-        <div className="col-span-2 lg:col-span-1 bg-neutral rounded-lg p-8 h-fit shadow-md">
+        <motion.div
+          variants={transitions.fadeIn}
+          initial="hidden"
+          whileInView="show"
+          className="col-span-2 lg:col-span-1 bg-neutral rounded-lg p-8 h-fit shadow-md"
+        >
           <h2 className="text-2xl sm:text-4xl font-semibold text-center mb-8 sm:leading-normal">
             Tell us about your project
           </h2>
           <p className="text-center mb-8">
-            How can AIAA help you? One of the team will be in touch after you
-            submit this form.
+            How can DigitizerSpace help you? One of the team will be in touch
+            after you submit this form.
           </p>
           <ContactForm />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
