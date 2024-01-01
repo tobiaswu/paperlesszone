31ebcecd1b8f3795504db19c0d7361949521cc2f
Tobias Wupperfeld <66420306+tobiaswu@users.noreply.github.com>
2024-01-01 20:50:30 -0300

    feat(post): add overlapping logo

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -10,49 +10,58 @@ export default function BlogPost({ params }: { params: { slug: string } }) {
 
   return (
     <>
-      {/* TODO: add overlapping image */}
-      <div className="glass p-8">
-        <div className="container mx-auto">
-          <div className="flex items-center justify-between">
-            <div className="badge badge-secondary badge-lg rounded-lg mb-2">
-              Trends
+      <div className="relative">
+        <div className="bg-neutral p-8">
+          <div className="container mx-auto">
+            <div className="flex items-center justify-between">
+              <div className="badge badge-secondary badge-lg rounded-lg mb-2">
+                Trends
+              </div>
+              {/* TODO: add share modal */}
+              <PostShareButton />
             </div>
-            {/* TODO: add share modal */}
-            <PostShareButton />
-          </div>
-          <h1 className="text-5xl font-bold mb-4 leading-tight">
-            The Paperless Office
-          </h1>
-          <p className="max-w-xl leading-relaxed">
-            Summary about paperless, all you need to know to implement it
-            successfully into your business operations. Learn how Chris F.
-            digitalized his real estate business.
-          </p>
-          <div className="flex gap-8 mt-8 items-center">
-            <p className="text-base self-end">Oct 19, 2023</p>
-            <div className="badge badge-primary badge-md rounded-lg">
-              5 mins read
+            <h1 className="text-5xl font-bold mb-4 leading-tight">
+              The Paperless Office
+            </h1>
+            <p className="max-w-xl leading-relaxed">
+              Summary about paperless, all you need to know to implement it
+              successfully into your business operations. Learn how Chris F.
+              digitalized his real estate business.
+            </p>
+            <div className="flex gap-8 mt-8 items-center">
+              <p className="text-base self-end">Oct 19, 2023</p>
+              <div className="badge badge-primary badge-md rounded-lg">
+                5 mins read
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className="mx-auto max-w-4xl py-16 px-4">
-        <h2 className="text-2xl font-semibold mb-4">Table of contents</h2>
-        <ol className="list-decimal list-inside">
-          <li>
-            <BlogContentRef
-              hash="section-1"
-              title="What is paperless office?"
-            />
-          </li>
-          <li>
-            <BlogContentRef hash="section-2" title="What are the benefits?" />
-          </li>
-          <li>
-            <BlogContentRef hash="section-3" title="Final thoughts" />
-          </li>
-        </ol>
+        <Image
+          className="container mx-auto md:absolute top-64 md:right-8 lg:right-16 xl:right-32 2xl:right-64 px-4 w-auto max-w-sm pt-8"
+          src="https://docs.paperless-ngx.com/assets/logo_full_white.svg"
+          alt="White logo of paperless ngx"
+          width={384}
+          height={184}
+        />
+
+        <div className="mx-auto max-w-4xl py-16 px-4">
+          <h2 className="text-2xl font-semibold mb-4">Table of contents</h2>
+          <ol className="list-decimal list-inside">
+            <li>
+              <BlogContentRef
+                hash="section-1"
+                title="What is paperless office?"
+              />
+            </li>
+            <li>
+              <BlogContentRef hash="section-2" title="What are the benefits?" />
+            </li>
+            <li>
+              <BlogContentRef hash="section-3" title="Final thoughts" />
+            </li>
+          </ol>
+        </div>
       </div>
 
       <div className="mx-auto max-w-4xl px-4">
