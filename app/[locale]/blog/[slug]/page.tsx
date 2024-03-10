3fabfbbef6c8242d2c5bdff8cb6f5b7981c89bd9
Tobias Wupperfeld <66420306+tobiaswu@users.noreply.github.com>
2024-03-10 10:21:34 -0300

    fix: center items

--- app/[locale]/blog/[slug]/page.tsx
+++ app/[locale]/blog/[slug]/page.tsx
@@ -131,7 +131,7 @@ export default async function Article({ params }: Props) {
             <p className="max-w-xl leading-relaxed">
               {article.attributes.description}
             </p>
-            <div className="flex flex-wrap gap-4 sm:gap-8 mt-8">
+            <div className="flex flex-wrap items-center gap-4 sm:gap-8 mt-8">
               <p className="text-base">
                 {t('blog.info.published') + formattedPublishedDate}
               </p>
