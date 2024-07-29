34e53ff1498152a0b225facbb3d66f9ecf793ce8
Tobias Wupperfeld <66420306+tobiaswu@users.noreply.github.com>
2024-07-29 13:32:14 +0300

    style: reduce progressbar height

--- components/MotionProgressbar.tsx
+++ components/MotionProgressbar.tsx
@@ -12,7 +12,7 @@ export const MotionProgressbar = () => {
 
   return (
     <motion.div
-      className="fixed top-0 left-0 right-0 h-2 bg-primary z-50 origin-left"
+      className="fixed top-0 left-0 right-0 h-1 bg-primary z-50 origin-left"
       style={{ scaleX }}
     ></motion.div>
   );
