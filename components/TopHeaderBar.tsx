d932610a8155a0072e3af16631f1c80bc785a7fe
Tobias Wupperfeld <66420306+tobiaswu@users.noreply.github.com>
2024-10-10 14:52:55 -0300

    style: center text

--- components/TopHeaderBar.tsx
+++ components/TopHeaderBar.tsx
@@ -22,8 +22,9 @@ export const TopHeaderBar = () => {
   if (!isVisible) return null;
 
   return (
-    <div className="w-full bg-sapphire-500 px-4 py-1 flex justify-between items-center gap-4 z-50">
-      <div className="flex items-center space-x-4">
+    <div className="w-full bg-sapphire-500 px-4 py-1 flex justify-between items-center z-50">
+      <div className="flex-1" />
+      <div className="flex items-center space-x-4 flex-grow justify-center">
         <span className="text-sm text-white">
           Get free weekly tips on open-source tools —&nbsp;
           <span className="font-bold">Join our newsletter!</span>
@@ -34,9 +35,11 @@ export const TopHeaderBar = () => {
           </button>
         </Link>
       </div>
-      <button onClick={handleClose}>
-        <PiXLight className="text-xl" />
-      </button>
+      <div className="flex-1 flex justify-end">
+        <button onClick={handleClose}>
+          <PiXLight className="text-xl text-white" />
+        </button>
+      </div>
     </div>
   );
 };
